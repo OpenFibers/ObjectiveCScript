b1eb1a29a890d136cb16c715ab47f279e4f61e4a
openthread <openfibers@gmail.com>
2013-10-24 00:24:54 +0800

    changed test case

--- ObjectiveCScriptDemo/OCSAppDelegate.m
+++ ObjectiveCScriptDemo/OCSAppDelegate.m
@@ -11,6 +11,32 @@
 #import "Expression.h"
 #import "ObjectiveCScriptParser.h"
 
+NSString *const classString =
+@"@implementation OCSAppDelegate"
+@"- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions"
+@"{"
+@"    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];"
+@"    // Override point for customization after application launch."
+@"    self.window.backgroundColor = [UIColor whiteColor];"
+@"    [self.window makeKeyAndVisible];"
+@"    "
+@"    [self test];"
+@"    "
+@"    return YES;"
+@"}"
+@"- (void)test"
+@"{"
+@"    CPTokenStream *tokenStream = [OCSTokeniser tokenise:@\"5llU + 5lu * 16f + (2.0f / 5.0u + 9l) * 8ll\"];"
+@"    NSLog(@\"tokenStream: %@\", tokenStream);"
+@"    "
+@"    CPParser *parser = [OCSParser parser];"
+@"    NSLog(@\"ANSWER %3.1f\", [(Expression *)[parser parse:tokenStream] value]);"
+@"}"
+@""
+@"@end";
+
+NSString *const expressionString = @"5llU + 5lu * 16f + (2.0f / 5.0u + 9l) * 8ll";
+
 @implementation OCSAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -27,7 +53,7 @@
      
 - (void)test
 {
-    CPTokenStream *tokenStream = [OCSTokeniser tokenise:@"5llU + 5lu * 16f + (2.0f / 5.0u + 9l) * 8ll"];
+    CPTokenStream *tokenStream = [OCSTokeniser tokenise:expressionString];
     NSLog(@"tokenStream: %@", tokenStream);
     
     CPParser *parser = [OCSParser parser];
