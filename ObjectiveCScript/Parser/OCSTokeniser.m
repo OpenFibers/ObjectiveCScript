4c4362e81d3824c377ccaf494842c06e2195f695
openthread <openfibers@gmail.com>
2013-10-24 00:24:46 +0800

    added 3 keyword to tokeniser

--- ObjectiveCScript/Parser/OCSTokeniser.m
+++ ObjectiveCScript/Parser/OCSTokeniser.m
@@ -96,6 +96,9 @@
                               @"@",
                               @",",
                               @".",
+                              @"=",
+                              @"\"",
+                              @"%",
                               ];
         [_tokeniser addTokenRecogniser:[CPKeywordRecogniser recogniserForKeywords:keywords]];
         
