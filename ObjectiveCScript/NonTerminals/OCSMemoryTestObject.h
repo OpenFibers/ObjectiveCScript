a3b760aa3911b71c08fc2d3b533d0fd1bd1a9e8d
openthread <openfibers@gmail.com>
2013-11-01 00:23:40 +0800

    added Memory Test Object

---
+++ ObjectiveCScript/NonTerminals/OCSMemoryTestObject.h
@@ -0,0 +1,13 @@
+//
+//  OCSTest.h
+//  ObjectiveCScriptDemo
+//
+//  Created by openthread on 11/1/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface OCSMemoryTestObject : NSObject
+
+@end
---
+++ ObjectiveCScript/NonTerminals/OCSMemoryTestObject.m
@@ -0,0 +1,28 @@
+//
+//  OCSTest.m
+//  ObjectiveCScriptDemo
+//
+//  Created by openthread on 11/1/13.
+//  Copyright (c) 2013 openthread. All rights reserved.
+//
+
+#import "OCSMemoryTestObject.h"
+
+@implementation OCSMemoryTestObject
+
+- (id)retain
+{
+    return [super retain];
+}
+
+- (oneway void)release
+{
+    [super release];
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
