ddd71eeff811a3fe980b386a53f93829d5003766
openthread <openfibers@gmail.com>
2013-11-03 23:06:00 +0800

    added 3 types to type keywords

--- ObjectiveCScript/Parser/OCSTokeniser.m
+++ ObjectiveCScript/Parser/OCSTokeniser.m
@@ -96,6 +96,9 @@
                                   @"long",
                                   @"float",
                                   @"double",
+                                  @"NSInteger",
+                                  @"NSUInteger",
+                                  @"CGFloat",
                                   @"nil",
                                   ];
         
