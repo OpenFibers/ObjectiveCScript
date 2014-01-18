17819757633d29f0eb23ca9def4dbbaf5f705f52
openthread <openfibers@gmail.com>
2014-01-19 02:29:03 +0800

    finished OCSMessageSend implementation

--- ObjectiveCScript/NonTerminals/OCSExecutableNonTerminals/OCSMessageSend/OCSMessageSend.h
+++ ObjectiveCScript/NonTerminals/OCSExecutableNonTerminals/OCSMessageSend/OCSMessageSend.h
@@ -9,6 +9,6 @@
 #import <Foundation/Foundation.h>
 #import <CoreParse/CoreParse.h>
 
-@interface OCSMethodSend : NSObject <CPParseResult>
+@interface OCSMessageSend : NSObject <CPParseResult>
 
 @end
---
+++ ObjectiveCScript/NonTerminals/OCSExecutableNonTerminals/OCSMessageSend/OCSMessageSend.m
@@ -0,0 +1,56 @@
+//
+//  OCSMethodSend.m
+//  ObjectiveCScriptDemo
+//
+//  Created by openthread on 18/1/14.
+//  Copyright (c) 2014 openthread. All rights reserved.
+//
+//@"OCSMessageSend ::="
+//  //method with args
+//  @"'[' ocsMessageReceiver@<OCSReturnableFactor> ocsMessageSendArgList@<OCSMessageSendArgList> ']' |"
+//  //method without args
+//  @"'[' ocsMessageReceiver@<OCSReturnableFactor> ocsMethodNameWithoutArg@'Identifier' ']' ;\n"
+
+#import "OCSMessageSend.h"
+#import "OCSReturnableFactor.h"
+#import "OCSMessageSendArgList.h"
+#import "OCSMessageSendArg.h"
+
+@implementation OCSMessageSend
+{
+    OCSReturnableFactor *_messageReceiver;
+    NSString *_ocsMethodName;
+    NSArray *_ocsMessageArgumentArray;
+}
+
+- (id)initWithSyntaxTree:(CPSyntaxTree *)syntaxTree
+{
+    self = [self init];
+    if (nil != self)
+    {
+        _messageReceiver = [syntaxTree valueForTag:@"ocsMessageReceiver"];
+        
+        OCSMessageSendArgList *ocsMessageArgList = [syntaxTree valueForTag:@"ocsMessageSendArgList"];
+        CPIdentifierToken *messageNameToken = [syntaxTree valueForTag:@"ocsMethodNameWithoutArg"];
+        if (messageNameToken)
+        {
+            _ocsMethodName = messageNameToken.identifier;
+            _ocsMessageArgumentArray = @[];
+        }
+        else if (ocsMessageArgList)
+        {
+            NSMutableString *methodName = [NSMutableString string];
+            NSMutableArray *methodArgs = [NSMutableArray array];
+            for (OCSMessageSendArg *ocsMethodSendArg in ocsMessageArgList.ocsMessageSendArgs)
+            {
+                [methodName appendString:ocsMethodSendArg.ocsMethodNamelet];
+                [methodArgs addObject:ocsMethodSendArg.ocsReturnableFactor];
+            }
+            _ocsMethodName = [NSString stringWithString:methodName];
+            _ocsMessageArgumentArray = [NSArray arrayWithArray:methodArgs];
+        }
+    }
+    return self;
+}
+
+@end
